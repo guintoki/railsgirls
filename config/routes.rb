dbaa5fc7a609e4c7b060d2dd9ecfe48a91dbef60
guintoki <guilherme.roberto@rdstation.com>
2022-06-08 17:04:36 -0300

    add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :comments
   get 'pages/info'
   resources :ideas
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

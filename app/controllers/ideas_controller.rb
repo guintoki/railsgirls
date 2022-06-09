840bcb682b669198e09c02c10de3be322e22d485
guintoki <guilherme.roberto@rdstation.com>
2022-06-09 14:48:16 -0300

    wip

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -23,8 +23,7 @@ class IdeasController < ApplicationController
 
   # POST /ideas or /ideas.json
   def create
-    binding.pry
-    @idea = Idea.new(idea_params)
+   @idea = Idea.new(idea_params)
 
     respond_to do |format|
       if @idea.save

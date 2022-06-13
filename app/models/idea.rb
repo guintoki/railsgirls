092b056b39b83e053e516ac1775fee6c0b676c95
guintoki <guilherme.roberto@rdstation.com>
2022-06-13 15:28:18 -0300

    add new tests

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,5 @@
 class Idea < ApplicationRecord
   has_many :comments
+  validates :name, presence: true
   mount_uploader :picture, PictureUploader
 end
--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe Idea, type: :model do
       second_idea = Idea.create!(name: "My Second Idea Name") # creating another new idea 'instance'
       expect(second_idea.name).to eq("My Second Idea Name") # this is our expectation
     end
-  end  
+
+    it "has a description" do
+      idea = Idea.create!(name: "My Idea Name", description: "My Awesome Idea Description")
+      second_idea = Idea.create!(name: "My Second Idea Name", description: "My Second Idea Description")
+      expect(second_idea.description).to eq("My Second Idea Description")
+    end
+  end
 end
